df74529dbf0ca8fdeb8fb28d5a8a24666d12071d
OumaArera <johnouma999@gmail.com>
2024-04-04 15:57:04 +0300

    feat: done

--- server/models.py
+++ server/models.py
@@ -1,54 +1,40 @@
-# server/models.py
-
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
 
-convention = {
-    "ix": "ix_%(column_0_label)s",
-    "uq": "uq_%(table_name)s_%(column_0_name)s",
-    "ck": "ck_%(table_name)s_%(constraint_name)s",
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-    "pk": "pk_%(table_name)s"
-}
+db = SQLAlchemy()
 
-metadata = MetaData(naming_convention=convention)
-
-db = SQLAlchemy(metadata=metadata)
-
-
-class Zookeeper(db.Model):
+class Zookeeper(db.Model, SerializerMixin):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, unique=True)
-    birthday = db.Column(db.Date)
+    name = db.Column(db.String)
+    birthday = db.Column(db.String)
 
-    animals = db.relationship('Animal', back_populates='zookeeper')
+    animals = db.relationship('Animal', backref='zookeeper')
 
+    def to_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
-class Enclosure(db.Model):
+class Enclosure(db.Model, SerializerMixin):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
     environment = db.Column(db.String)
     open_to_visitors = db.Column(db.Boolean)
 
-    animals = db.relationship('Animal', back_populates='enclosure')
+    animals = db.relationship('Animal', backref='enclosure')
 
+    def to_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
-class Animal(db.Model):
+class Animal(db.Model, SerializerMixin):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, unique=True)
+    name = db.Column(db.String)
     species = db.Column(db.String)
-
     zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id'))
     enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'))
 
-    enclosure = db.relationship('Enclosure', back_populates='animals')
-    zookeeper = db.relationship('Zookeeper', back_populates='animals')
-
-    def __repr__(self):
-        return f'<Animal {self.name}, a {self.species}>'
+    def to_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
